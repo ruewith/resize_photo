b919ebc34378828f195fd7d75429520080d19183
ruewith <ruewith@gmail.com>
2019-12-03 14:02:20 +0300

    commit

--- create.php
+++ create.php
@@ -1,5 +1,16 @@
 <?php 
-include 'image.inc.php';
-header('Content-Type: image/png'); //или /png /gif, т.е то что нам надо
-create_thumbnail('test.jpg', '1.png', 200, 200);
-create_thumbnail('test2.jpg', '2.png', 200, 200);
+include_once 'image.inc.php';
+header('Content-Type: image/jpeg'); //или /png /gif, т.е то что нам надо
+$user = 255;
+$path = "src/test.jpg";
+
+$sizes = array();
+
+$sizes["small"] = array("W"=>140, "H"=>105);
+$sizes["clip"] = array("W"=>40, "H"=>40);
+$sizes["clip2"] = array("W"=>80, "H"=>80);
+
+foreach ($sizes as $size => $out) {
+    resize_photo($path, "images/".$size."/".$user.".jpg", $out);
+}
+
--- image.inc.php
+++ image.inc.php
@@ -1,11 +1,12 @@
 <?php 
-    function create_thumbnail($path, $save, $width, $height) {
-        $info = getimagesize($path); //получаем размеры картинки и ее тип
-        $size = array($info[0], $info[1]); //закидываем размеры в массив
+    function resize_photo($path, $save, $output) {
+        $info = getimagesize($path);
+        $size = array($info[0], $info[1]);
+        $width = $output["W"];
+        $height = $output["H"];
      
-            //В зависимости от расширения картинки вызываем соответствующую функцию
         if ($info['mime'] == 'image/png') {
-            $src = imagecreatefrompng($path); //создаём новое изображение из файла
+            $src = imagecreatefrompng($path);
         } else if ($info['mime'] == 'image/jpeg') {
             $src = imagecreatefromjpeg($path);
         } else if ($info['mime'] == 'image/gif') {
@@ -14,24 +15,19 @@
             return false;
         }
      
-        $thumb = imagecreatetruecolor($width, $height); //возвращает идентификатор изображения, представляющий черное изображение заданного размера
-        $src_aspect = $size[0] / $size[1]; //отношение ширины к высоте исходника
-        $thumb_aspect = $width / $height; //отношение ширины к высоте аватарки
+        $thumb = imagecreatetruecolor($width, $height);
+        $src_aspect = $size[0] / $size[1];
+        $thumb_aspect = $width / $height;
      
-        if($src_aspect < $thumb_aspect) {        
-            //узкий вариант (фиксированная ширина)      
+        if($src_aspect < $thumb_aspect) { 
             $scale = $width / $size[0];         
             $new_size = array($width, $width / $src_aspect);        
-            $src_pos = array(0, ($size[1] * $scale - $height) / $scale / 2); 
-            //Ищем расстояние по высоте от края картинки до начала картины после обрезки   
+            $src_pos = array(0, ($size[1] * $scale - $height) / $scale / 2);   
         } else if ($src_aspect > $thumb_aspect) {
-            //широкий вариант (фиксированная высота)
             $scale = $height / $size[1];
             $new_size = array($height * $src_aspect, $height);
             $src_pos = array(($size[0] * $scale - $width) / $scale / 2, 0); 
-            //Ищем расстояние по ширине от края картинки до начала картины после обрезки
         } else {
-            //другое
             $new_size = array($width, $height);
             $src_pos = array(0,0);
         }
@@ -40,12 +36,6 @@
         $new_size[1] = max($new_size[1], 1);
      
         imagecopyresampled($thumb, $src, 0, 0, $src_pos[0], $src_pos[1], $new_size[0], $new_size[1], $size[0], $size[1]);
-        //Копирование и изменение размера изображения с ресемплированием
      
-        if($save === false) {
-            return imagepng($thumb); //Выводит JPEG/PNG/GIF изображение
-        } else {
-            return imagepng($thumb, $save);//Сохраняет JPEG/PNG/GIF изображение
-        }
-     
+        return imagejpeg($thumb, $save);
     }
